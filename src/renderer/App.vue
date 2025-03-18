d1f6d40600479e79d2566c73e590889735e94bb3
zero <zero.kwok@foxmail.com>
2025-03-18 19:47:45 +0800

    Update App.vue to support swappable window frames, tweak styles, and optimize configurations

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,59 +1,68 @@
-
 <template>
-  <WindowFrame title="标题栏文本: 内容太大而无法放入指定区域时, 将自动省略">
-    <header>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </header>
-    <div class="wrapper">
-      <RouterView />
-    </div>
-  </WindowFrame>
+    <component :is="windowFrameComponent">
+      <header>
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+          <label>
+            <input type="checkbox" v-model="useRoundedFrame"/> Toggle Frame
+          </label>
+      </header>
+      <div class="container">
+        <RouterView />
+      </div>
+      </component>
 </template>
 
 <script setup>
+import { ref, computed, defineAsyncComponent } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
-// import WindowFrame from './components/WindowFrame.vue'
-import WindowFrame from './components/RoundedWindowFrame.vue'
+
+const useRoundedFrame = ref(false);
+const windowFrameComponent = computed(() => {
+  return useRoundedFrame.value
+    ? defineAsyncComponent(() => import('./components/RoundedWindowFrame.vue'))
+    : defineAsyncComponent(() => import('./components/WindowFrame.vue'))
+});
 </script>
 
 <style lang="scss" scoped>
-
 header {
   height: 32px;
   line-height: 1.5;
   display: flex;
   place-items: center;
-}
-
-nav {
-  width: 100%;
   font-size: 12px;
-  text-align: center;
+  nav {
+    width: 100%;
+    text-align: center;
+    a {
+      display: inline-block;
+      padding: 0 1rem;
+      border-left: 1px solid var(--color-border);
+      &:first-of-type {
+        border: 0;
+      }
+      &.router-link-exact-active {
+        color: var(--color-text);
+        &:hover {
+          background-color: transparent;
+        }
+      }
+    }
+  }
+  label {
+    display: flex;
+    white-space: nowrap;
+    padding: 0 10px;
+    input {
+      margin-right: 5px;
+    }
+  }
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.container {
+  height: calc(100% - 32px);
 }
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-.wrapper {
-  height: calc(100% -  32px);
-}
-
 </style>
