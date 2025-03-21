1d13cf187e5bfa3d91770acff9c6e400c59d4e28
zero <zero.kwok@foxmail.com>
2025-03-21 10:28:55 +0800

    Add the internationalization function based on vue-i18n

---
+++ src/renderer/i18n.js
@@ -0,0 +1,52 @@
+
+import { nextTick } from 'vue'
+import { createI18n } from 'vue-i18n';
+import store from '#/store/state.js';
+
+const i18n = createI18n({
+    legacy: false,
+    locale: store.state.settings.language,
+    fallbackLocale: 'en',
+    messages: {},
+});
+
+async function loadLocale(i18n, locale) {
+    if (i18n.global.availableLocales.includes(locale))
+        return;
+
+    const locales = i18n.global.index.locales;
+    if (!locale in locales)
+        throw new Error('Invalid locale');
+
+    const file = locales[locale].file;
+    const module = await import(`./locales/${file}`);
+    i18n.global.setLocaleMessage(locale, module.default);
+};
+
+async function setLocale(i18n, locale) {
+    if (!i18n.global.availableLocales.includes(locale))
+        throw new Error('Invalid locale');
+
+    document.querySelector('html').setAttribute('lang', locale);
+    i18n.global.locale.value = locale;
+    return nextTick();
+}
+
+await import('./locales/index.js').then(async (module) => {
+    const index = module.default;
+    i18n.global.index = index;
+    i18n.global.fallbackLocale = index.fallbackLocale;
+
+    await loadLocale(i18n, i18n.global.locale.value);
+    nextTick();
+});
+
+store.watch(
+    (state) => state.settings.language,
+    async (language) => {
+        await loadLocale(i18n, language);
+        await setLocale(i18n, language);
+    }
+);
+
+export default i18n;
--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -31,10 +31,12 @@ import './index.css';
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from '../shared/store/state'
+import store from '#/store/state'
+import i18n from './i18n'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(store)
+app.use(i18n)
 app.mount('#app')
