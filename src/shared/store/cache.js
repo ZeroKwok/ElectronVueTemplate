1bbc5e43717d3fd688fa3fad37886e51e3623d21
zero <zero.kwok@foxmail.com>
2025-03-24 18:21:14 +0800

    Refactor cache

--- src/shared/store/cache.js
+++ src/shared/store/cache.js
@@ -1,10 +1,52 @@
-class Cache extends Map {
-    get(key, defaultValue) {
-        if (this.has(key)) {
-            return super.get(key);
-        }
-        return defaultValue;
-    }
-}
+class Cache {
+  constructor() {
+    this._store = new Map();
+    this._listeners = new Map();
+  }
 
-export default new Cache()
+  set(key, value) {
+    const oldValue = this._store.get(key);
+    if (oldValue === value) 
+      return this;
+    this._store.set(key, value);
+    this._emit('set', key, value, oldValue);
+    return this;
+  }
+
+  get(key, defaultValue) {
+    if (this._store.has(key))
+      return this._store.get(key);
+    return defaultValue;
+  }
+
+  delete(key) {
+    if (this._store.has(key)) {
+      const oldValue = this._store.get(key);
+      this._store.delete(key);
+      this._emit('delete', key, undefined, oldValue);
+      return true;
+    }
+    return false;
+  }
+
+  on(event, callback) {
+    if (!this._listeners.has(event))
+      this._listeners.set(event, new Set());
+    this._listeners.get(event).add(callback);
+    return this;
+  }
+
+  off(event, callback) {
+    if (this._listeners.has(event))
+      this._listeners.get(event).delete(callback);
+    return this;
+  }
+
+  _emit(event, key, value, oldValue) {
+    const listeners = this._listeners.get(event);
+    if (listeners)
+      listeners.forEach(cb => cb(key, value, oldValue));
+  }
+};
+
+export default new Cache();
