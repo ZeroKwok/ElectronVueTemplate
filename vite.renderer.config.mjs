30c7f17ffe0c239e696f4e20638a914cc75bcb24
zero <zero.kwok@foxmail.com>
2025-03-25 11:26:52 +0800

    Add vite-plugin-vue-devtools

--- vite.renderer.config.mjs
+++ vite.renderer.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config
 export default defineConfig({
     plugins: [
-        vue()
+        vue(),
+        VueDevTools(),
     ],
     resolve: {
         alias: {
