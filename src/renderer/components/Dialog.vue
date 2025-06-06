ddb69ff83d261ab4fe2b82bd240e7a0e64bcf87d
zero <zero.kwok@foxmail.com>
2025-06-06 19:07:15 +0800

    feat(Dialog): Add dialog components to support draggable and modal functionality

---
+++ src/renderer/components/Dialog.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="dialog">
+    <el-dialog model-value="true" :show-close="false" :modal="modal" :draggable="draggable"
+      :close-on-click-modal="false">
+      <template #header="{ close, titleId, titleClass }">
+        <div class='titlebar'>
+          <div v-if="$slots.icon">
+            <slot name="icon"></slot>
+          </div>
+          <div :id="titleId" :class="['title', titleClass]">{{ title }}</div>
+          <div class="button close codicon codicon-chrome-close" @click="close"></div>
+        </div>
+      </template>
+      <div class="container">
+        <slot>Client Area</slot>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: 'title',
+  },
+  draggable: {
+    type: Boolean,
+    default: true,
+  },
+  modal: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+:deep(.el-dialog) {
+  border-radius: var(--window-border-radius);
+  ;
+}
+
+.titlebar {
+  display: flex;
+  gap: 5px;
+  height: 40px;
+  align-items: center;
+  padding-left: 10px;
+
+  .title {
+    font-size: 14px;
+    margin-right: auto;
+  }
+
+  .button {
+    height: 100%;
+    width: 46px;
+    font-size: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: default;
+    border-top-right-radius: var(--window-border-radius);
+
+    &:hover {
+      background: #c42c1b;
+      color: rgba(255, 255, 255, 0.8);
+    }
+
+    &:active {
+      background: #f1707a;
+      color: #000;
+    }
+  }
+}
+
+.container {
+  padding: 20px;
+}
+</style>
